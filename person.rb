8138c6c73805b458230b555e66fec80e080af011
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-06 14:03:23 +0300

    Add decorators to my class

--- person.rb
+++ person.rb
@@ -1,7 +1,15 @@
-require_relative './student'
-require_relative './teacher'
+# require_relative './student'
+# require_relative './teacher'
 
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
+
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -16,10 +24,41 @@ class Person
     of_age or @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age()
     return true if @age >= 18
+  end
+end
+
+class Decorator < Nameable
+  attr_accessor :nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+def correct_name
+  @nameable.correct_name
+end
+
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize()
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.length > 10
+      return @nameable.correct_name[0..9]
+    end
+    @nameable.correct_name
   end
 end
 
