175c41717716adc4c0428cb1d82fc22d613e9367
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-06 15:54:37 +0300

    Test

--- person.rb
+++ person.rb
@@ -16,7 +16,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = true
-    super
+    
   end
 
   def can_use_services()
@@ -39,7 +39,6 @@ class Decorator < Nameable
 
   def initialize(nameable)
     @nameable = nameable
-    super
   end
 
   def correct_name
@@ -55,10 +54,13 @@ end
 
 class TrimmerDecorator < Decorator
   def correct_name
-    if @nameable.correct_name.length > 10
-      return @nameable.correct_name[0..9]
-    end
-
-    @nameable.correct_name
+    @nameable.correct_name[0..9] if @nameable.correct_name.length > 10 || @nameable.correct_name
   end
 end
+
+person = Person.new(22, 'maximilianu99999s')
+p person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+p capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+p capitalizedTrimmedPerson.correct_name
