b53fd37a443fa285836b717bec59a89979147154
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-08 20:14:54 +0300

    main.rb

--- main.rb
+++ main.rb
@@ -1,23 +1,21 @@
-require_relative './app'
+require './app'
+require './json_db'
 
 def print_options
-  options = [
-    'List all books',
-    'List all people',
-    'Create a person',
-    'Create a book',
-    'Create a rental',
-    'List all rentals for a given person id',
-    'Save & Exit'
-  ]
-  put 'Please select an option by entering a number for that option'
+  options = ['List all books',
+             'List all people',
+             'Create a person',
+             'Create a book',
+             'Create a rental',
+             'List all rentals for a given person id',
+             'Save & Exit']
+  puts 'Please choose an option by entering a number:'
   options.each_with_index do |option, index|
     puts "#{index + 1} - #{option}"
   end
 end
 
-
-def execute_option(option, app)
+def execute_option(option, app) # rubocop:disable Metrics/CyclomaticComplexity
   case option
   when '1'
     app.list_books
@@ -25,7 +23,6 @@ def execute_option(option, app)
     app.list_people
   when '3'
     app.create_person
-
   when '4'
     app.add_book
   when '5'
@@ -35,13 +32,13 @@ def execute_option(option, app)
   when '7'
     return true
   else
-    puts "Invalid option"
+    puts 'Invalid option'
   end
   false
 end
 
 def main
-  puts "Welcome to school library"
+  puts 'Welcome to School Library App!'
   app = App.new
   load_state(app)
   exit = false
@@ -50,8 +47,7 @@ def main
     exit = execute_option(gets.chomp, app)
   end
   save_state(app)
-  puts "Thank you for using our library"
+  puts 'Thank you for using this app!'
 end
 
 main
-
