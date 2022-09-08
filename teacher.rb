d5cab4b4afe751762bab07b4d957fddc543d7fa9
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-08 20:19:42 +0300

    teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,14 @@
-require_relative './person'
+require './person'
 
 class Teacher < Person
+  attr_accessor :specialization
+
   def initialize(age, specialization, parent_permission: true, name: 'Unknown', id: SecureRandom.hex(5))
     super(age, parent_permission: parent_permission, name: name, id: id)
     @specialization = specialization
   end
 
-  def can_use_services()
+  def can_use_services?
     true
   end
 end
