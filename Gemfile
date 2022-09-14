f9b72d659430c3c1ef37241dafe67fedfc3c1d84
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-14 21:58:56 +0300

    fix linters

--- Gemfile
+++ Gemfile
@@ -1,7 +1,3 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-# gem "rails"
+source 'https://rubygems.org'
 
 gem 'rspec', '~>3.9'
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 require './student'
 
 describe 'Student' do
-  student1 = Student.new(21, "Adyo")
+  student1 = Student.new(21, 'Adyo')
   it 'Test self' do
     expect(student1.class).to be(Student)
   end
